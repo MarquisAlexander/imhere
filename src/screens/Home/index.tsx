b68d8d633d654f5123f8de24c61b2bd7cd203b55
MarquisAlexander <marquis.alexander@outlook.com>
2022-10-03 20:38:13 -0300

    feat():new components

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+
+import { Participant } from "../../components/Participant";
+
+import { styles } from "./styles";
+
+export function Home() {
+	function handleParticipantAdd() {
+		console.log("voce clicou no botão de adicionar");
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.eventName}>Nome do Evento</Text>
+			<Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+			<View style={styles.form}>
+				<TextInput
+					style={styles.input}
+					placeholder="Nome do participante"
+					placeholderTextColor="#6B6B6B"
+				/>
+
+				<TouchableOpacity onPress={handleParticipantAdd} style={styles.button}>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableOpacity>
+			</View>
+			<Participant />
+		</View>
+	);
+}
